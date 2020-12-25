3625000f0e4a8bbb020001a4dda887a26656ed39
manucho <manuel.blanco77@gmail.com>
2020-12-25 14:41:23 -0300

    notificar al haber un cliente conectado + actualizar socket.io para que tenga la misma version que la app de frontend

--- models/sockets.js
+++ models/sockets.js
@@ -13,6 +13,8 @@ class Sockets {
         // On connection
         this.io.on('connection', ( socket ) => {
 
+            console.log('cliente conectado');
+
             //TODO: Validar JWT 
             //Si token no es valido, desconectarlo
 
